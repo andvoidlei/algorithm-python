9adc42bc9cc818261caaeb01e670429c16c9ab08
wuxiaolei <wuxiaolei@lvmama.com>
2018-06-21 20:25:10 +0800

    modify by wxl

--- test/test_file.py
+++ test/test_file.py
@@ -1,14 +1,32 @@
 
 import numpy as np
 
-matrix = np.zeros((1, 1))
-for line in open("D:/aaa.txt"):
+matrix = np.zeros((10, 12))
+for line in open("./../data/aaa.txt"):
     print(line)
     print(line.split())
-    user, item, _ = line.split(',')
+    user, item, _,_ = line.split(',')
     user = int(user)
     item = int(item)
     count = 1.0
-    print(user,item)
-    count = 1.0
     matrix[user-1,item-1] = count
+    print(matrix)
+
+class A(object):
+    def __method(self):
+        print ("I'm a method in A")
+    def method(self):
+        self.__method()
+
+class B(A):
+    def __method(self):
+        print ("I'm a method in B")
+
+b = B()
+b.method()
+
+#a = A()
+#a.method()
+
+
+
